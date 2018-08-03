b4bfbc30eafb9eb0f6446e54c51f0415bb4672da
4KaNE <4KaNEwork@gmail.com>
2018-08-03 16:27:55 +0900

    glanq.sqlに宛先情報テーブルを追加

--- sql/glanq.sql
+++ sql/glanq.sql
@@ -112,3 +112,28 @@ foreign key(product_id) references product_info(product_id)
 
 default charset=utf8
 comment="購入履歴情報テーブル";
+
+-- 宛先情報テーブルの作成
+/*
+ * 備考
+ * IDはpk, auto_increment
+ */
+
+create table destination_info(
+id int primary key not null auto_increment comment "ID",
+user_id varchar(16) not null comment "ユーザーID",
+family_name varchar(32) not null comment "姓",
+first_name varchar(32) not null comment "名",
+family_name_kana varchar(32) not null comment "姓かな",
+first_name_kana varchar(32) not null comment "名かな",
+email varchar(32) not null comment "メールアドレス",
+tel_number varchar(13) not null comment "電話番号",
+user_address varchar(50) not null comment "住所",
+regist_date datetime not null comment "登録日",
+update_date datetime not null comment "更新日"
+)
+
+default charset=utf8
+comment="宛先情報テーブル";
+
+-- 宛先情報のinsert文を挿入
