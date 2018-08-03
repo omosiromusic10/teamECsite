d5db7ab7d1b459edd717bbbd108db5eb57aea8ac
4KaNE <4KaNEwork@gmail.com>
2018-08-03 15:52:07 +0900

    glanq.sqlに商品情報テーブルを追加

--- sql/glanq.sql
+++ sql/glanq.sql
@@ -28,4 +28,29 @@ update_date datetime not null comment "更新日"
 default charset=utf8
 comment="会員情報テーブル";
 
--- 会員情報テーブルのinsert文を挿入
+-- 会員情報のinsert文を挿入
+
+-- 商品情報テーブル作成
+
+create table product_info(
+id int primary key not null auto_increment comment "ID",
+product_id int unique not null comment "商品ID",
+product_name varchar(100) unique not null comment "商品名",
+product_name_kana varchar(100) unique not null comment "商品名かな",
+product_description varchar(255) not null comment "商品詳細",
+category_id int not null not null comment "カテゴリID",
+price int comment "価格",
+image_file_path varchar(100) comment "画像ファイルパス",
+image_file_name varchar(50) comment "画像ファイル名",
+release_date datetime not null comment "発売年月",
+release_company varchar(50) comment "発売会社",
+status tinyint not null default 0 comment "ステータス",
+regist_date datetime not null comment "登録日",
+update_date datetime comment "更新日",
+foreign key(category_id) references m_category(category_id)
+)
+
+default charset=utf8
+comment="商品情報テーブル";
+
+-- 商品情報のinsert文を追加
