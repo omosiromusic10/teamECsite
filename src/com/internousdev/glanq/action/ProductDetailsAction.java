57cf18f7b521572a799a83663eeba54cdc53d9b6
HirokiYuri-0315 <timidhy@yahoo.co.jp>
2018-08-03 17:54:29 +0900

    微修正

--- src/com/internousdev/glanq/action/ProductDetailsAction.java
+++ src/com/internousdev/glanq/action/ProductDetailsAction.java
@@ -1,24 +1,47 @@
 package com.internousdev.glanq.action;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.glanq.dao.ProductInfoDAO;
+import com.internousdev.glanq.dto.ProductInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductDetailsAction extends ActionSupport implements SessionAware {
 
 	public Map<String, Object> session;
+	private int productId;
+	private ProductInfoDTO productInfoDTO;
 
 	// 作成中です・・・
-	public String execute() {
-		String result = ERROR;
+	public String execute() throws SQLException {
+		ProductInfoDAO pDAO1 = new ProductInfoDAO();
+		setProductInfoDTO(pDAO1.getProductInfo(productId));
+		session.put("productInfoDTO", productInfoDTO);
+
+		String result = SUCCESS;
 		return result;
 	}
 
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public ProductInfoDTO getProductInfoDTO() {
+		return productInfoDTO;
+	}
+	public void setProductInfoDTO(ProductInfoDTO productInfoDTO) {
+		this.productInfoDTO = productInfoDTO;
 	}
 
 
