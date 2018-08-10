b405e979021a15b9ffaa204a1dd2de74523a3877
4KaNE <4KaNEwork@gmail.com>
2018-08-10 16:27:37 +0900

    LocationChoiceActionを変更

--- src/com/internousdev/glanq/action/LocationChoiceAction.java
+++ src/com/internousdev/glanq/action/LocationChoiceAction.java
@@ -1,9 +1,15 @@
 package com.internousdev.glanq.action;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.glanq.dao.DestinationInfoDAO;
+import com.internousdev.glanq.dto.DestinationInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LocationChoiceAction extends ActionSupport implements SessionAware{
@@ -11,9 +17,24 @@ public class LocationChoiceAction extends ActionSupport implements SessionAware{
 	private String categoryId;
 	private Map<String, Object> session;
 
+	DestinationInfoDAO destinationInfoDAO = new DestinationInfoDAO();
+	List<DestinationInfoDTO> destinationInfoDtoList = new ArrayList<>();
+
 	public String execute(){
 		/*画面遷移のみ*/
 		String result = ERROR;
+
+		try {
+			destinationInfoDtoList = destinationInfoDAO.getDestinationInfo("admin");
+			Iterator<DestinationInfoDTO> iterator = destinationInfoDtoList.iterator();
+			if(!(iterator.hasNext())) {
+				destinationInfoDtoList = null;
+			}
+			session.put("destinationInfoDtoList", destinationInfoDtoList);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		result = SUCCESS;
 
 		return result;
