ebfe452893985835b399187cdb0832203f1479e3
kashitani0530 <ksy05x30inter@yahoo.co.jp>
2018-08-16 17:38:57 +0900

    errorメッセージ変更

--- src/com/internousdev/glanq/util/InputChecker.java
+++ src/com/internousdev/glanq/util/InputChecker.java
@@ -142,7 +142,7 @@ public class InputChecker {
 	public List<String> doPasswordCheck(String newPassword, String reConfirmationPassward){
 		List<String> resultList = new ArrayList<String>();
 		if(!(newPassword.equals(reConfirmationPassward))){
-			resultList.add("入力されたパスワードが異なります。");
+			resultList.add("確認用パスワードが異なります。");
 		}
 		/**
 		 * 引数として渡された2つのパスワードが等しいかどうか
