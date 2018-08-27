c3f5f1ef243d0e0fdbf1408ab07aaf056cf4a4d7
4KaNE <4KaNEwork@gmail.com>
2018-08-27 16:20:11 +0900

    商品IDを元に購入履歴を削除するメソッドを追加

--- src/com/internousdev/glanq/dao/PurchaseHistoryInfoDAO.java
+++ src/com/internousdev/glanq/dao/PurchaseHistoryInfoDAO.java
@@ -261,4 +261,32 @@ public class PurchaseHistoryInfoDAO {
 		}
 		return count;
 	}
+
+
+	// 特定の商品IDを持つ購入履歴を全て削除
+	public int deleteBasedonPid(int product_id){
+		DBConnector dbConnector = new DBConnector();
+		Connection connection = dbConnector.getConnection();
+
+		/* purchase_history_infoのproduct_idに該当するデータがあれば削除 */
+		String sql = "delete from purchase_history_info where user_id=?";
+		int count = 0;
+
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, product_id);
+
+			/* count = 更新回数 */
+			count = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
+	}
 }
